e7b31db280e0a7e6fcb68254586edff10df3785b
Emerson <eyenessus@gmail.com>
2025-04-28 20:12:42 -0300

    fix: listagem de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -48,7 +48,34 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "SELECT * FROM produtos";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
 
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        } finally {
+            try {
+                if (conn != null)
+                    conn.close();
+                if (prep != null)
+                    prep.close();
+                if (resultset != null)
+                    resultset.close();
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
         return listagem;
     }
 
