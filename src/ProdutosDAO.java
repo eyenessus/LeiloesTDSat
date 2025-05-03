242197ec11ea0ef880f53d1ad7e235e760834455
Emerson <eyenessus@gmail.com>
2025-05-03 20:24:51 -0300

    feat: listagem de pedidos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -103,4 +103,36 @@ public class ProdutosDAO {
             }
         }
     }
+
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos vendidos: " + e.getMessage());
+        } finally {
+            try {
+                if (conn != null)
+                    conn.close();
+                if (prep != null)
+                    prep.close();
+                if (resultset != null)
+                    resultset.close();
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage());
+            }
+        }
+        return listagem;
+    } 
 }
